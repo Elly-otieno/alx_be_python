459f2e796346c0f682db47b4481d8eebf7229f07
Elly-otieno <okothelli@gmail.com>
2025-09-10 15:28:49 +0300

    redoing basic operations

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,10 +1,21 @@
+# arithmetic operations
+ 
 number1 = 10
 number2 = 5
 
+# addition
 sum = number1 + number2
-difference = number1 - number2
+
+# subtration
+diff = number1 - number2
+
+# multiplication
 product = number1 * number2
 
+
+# results
+#print("Addition of", number1, "and", number2, "is", sum )
+
 print(f'Addition of {number1} and {number2} is {sum}')
-print(f'Subtraction of {number1} and {number2} is {difference}')
-print(f'Multiplication of {number1} and {number2} is {product}')
+print("Subtration of", number1, "and", number2, "is", diff )
+print("Multiplication of", number1, "and", number2, "is", product )
