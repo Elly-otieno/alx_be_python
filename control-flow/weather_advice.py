7f2c38a07d28a35be5f0ab0b9bfe65f27edc5071
Elly-otieno <okothelli@gmail.com>
2025-09-21 23:35:30 +0300

    changes

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -4,11 +4,11 @@ Utilizing conditional statements to to guide program execution based on the inpu
 
 condition = input("What's the weather like today? (sunny/rainy/cold): ")
 
-if condition == 'sunny':
+if condition == "sunny":
     recomend = "Wear a t-shirt and sunglasses."
-elif condition == 'rainy':
+elif condition == "rainy":
     recomend = "Don't forget your umbrella and a raincoat."
-elif condition == 'cold':
+elif condition == "cold":
     recomend = 'Make sure to wear a warm coat and a scarf.'
 else:
     recomend = "Sorry, I don't have recommendations for this weather."
