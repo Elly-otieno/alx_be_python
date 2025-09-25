eb8b1b1391bc003424425c890533599c7494d14c
Elly-otieno <okothelli@gmail.com>
2025-09-25 16:13:16 +0300

    updating daily_reminder

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -43,23 +43,23 @@ time_bound = input('Is it time-bound? (yes/no): ')
 
 match priority:
     case 'high':
-        if time_bound.lower() == 'yes':
+        if time_bound == 'yes':
             print(f"Reminder: '{task}' is a high priority task that requires immediate attention today!")
-        elif time_bound.lower() == 'no':
+        elif time_bound == 'no':
             print(f"Reminder: '{task}' is a high priority task. Consider completing it when you have free time.")
         else:
             print(f"Invalid time bound for task '{task}'")
     case 'medium':
-        if time_bound.lower() == 'yes':
+        if time_bound == 'yes':
             print(f"Reminder: '{task}' is a medium priority task that requires immediate attention today!")
-        elif time_bound.lower() == 'no':
+        elif time_bound == 'no':
             print(f"Reminder: '{task}' is a medium priority task. Consider completing it when you have free time.")
         else:
             print(f"Invalid time bound for task '{task}'")
     case 'low':
-        if time_bound.lower() == 'yes':
+        if time_bound == 'yes':
             print(f"Note: '{task}' is a low priority task that requires immediate attention")
-        elif time_bound.lower() == 'no':
+        elif time_bound == 'no':
             print(f"Note: '{task}' is a low priority task. Consider completing it when you have free time.")
         else:
             print(f"Invalid time bound for task '{task}'")
