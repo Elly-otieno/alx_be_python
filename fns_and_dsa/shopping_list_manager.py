8851616fc646cd11463419faf4c3600dd3be983c
Elly-otieno <okothelli@gmail.com>
2025-09-28 21:04:45 +0300

    updating shopping list

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -32,10 +32,10 @@ def main():
         choice = input('Enter your choice: ')
 
         if choice == '1':
-            item = input('Enter an item to add to the shopping list: ')
+            item = input('Enter the item to add: ')
             shopping_list.append(item)
         elif choice == '2':
-            item = input('Enter an item to remove from the shopping list: ')
+            item = input('Enter the item to remove: ')
             if item in shopping_list:
                 shopping_list.remove(item)
             else:
