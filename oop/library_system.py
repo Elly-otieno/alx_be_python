1f87e0578e39f5326cc6053afd96decd09b921a7
Elly-otieno <okothelli@gmail.com>
2025-10-12 22:18:37 +0300

    inheritance & composition

--- oop/library_system.py
+++ oop/library_system.py
@@ -19,3 +19,30 @@ Methods:
 add_book(self, book): Adds a Book, EBook, or PrintBook instance to the library.
 list_books(self): Prints details of each book in the library.
 '''
+
+class Book:
+    def __init__(self, title:str, author:str):
+        self.title = title
+        self.author = author
+
+class EBook(Book):
+    def __init__(self, title, author, file_size):
+        super().__init__(title, author)
+        self.file_size = file_size
+
+
+class PrintBook(Book):
+    def __init__(self, title, author, page_count):
+        super().__init__(title, author)
+        self.page_count = page_count
+
+class Library:
+    def __init__(self):
+        self.books = []
+
+    def add_book(self, book):
+        self.books.append(book)
+    
+    def list_books(self):
+        for book in self.books:
+            print(book)
